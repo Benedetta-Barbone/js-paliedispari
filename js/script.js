99c6738994999754dda7cfa0ff0bb6bd76013827
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-16 16:58:49 +0200

    inserite le due funzioni per sommare i numeri e generare un numero random

--- js/script.js
+++ js/script.js
@@ -11,3 +11,32 @@ if (palidroma(parola)){
 }else{
   console.log('non è un palidroma')
 }
+
+
+
+
+function numeroRandom() {
+  Math.floor(Math.random() * 5 + 1);
+}
+
+
+function sommaNumeri (num1, num2){
+  let somma = num1 + num2
+  if (somma % 2 === 0){
+    return 'pari'
+  }else{
+    return 'dispari'
+  }
+}
+
+
+const pariDispari = prompt('Scegli pari o dispari');
+const userNumber = Number.parseInt(prompt('inserisci un numero da 1 a 5'), 10);
+const computerNumber = numeroRandom();
+
+
+if (sommaNumeri(userNumber, computerNumber)==pariDispari){
+  console.log('hai vinto')
+}else{
+  console.log('hai perso')
+}
